1e0d5cc21eec97452abfe3bdc66141d47795bfed
peyo <peteryyoon@gmail.com>
2020-05-22 19:23:34 -0700

    connecting to heroku

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da74db5e506c764b75cba9a07895b706.js"
+  "/precache-manifest.7d4f30a48bb12f12565d4f1533a136aa.js"
 );
 
 self.addEventListener('message', (event) => {
