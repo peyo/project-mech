ff1c039533afd9c8e810ba092b8d5bc115d18d12
peyo <peteryyoon@gmail.com>
2020-05-26 10:26:30 -0700

    added demo login credentials to the welcome page.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6dec44b54d2e8d48924673501f66dd41.js"
+  "/precache-manifest.9c5e8c3f43ac3f3ede99389ee82177a3.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/routes/WelcomePage/WelcomePage.js
+++ src/routes/WelcomePage/WelcomePage.js
@@ -5,28 +5,41 @@ import "./WelcomePage.css";
 export default class WelcomePage extends Component {
   render() {
     return (
-        <main className="WelcomePage__main">
-          <div className="WelcomePage__screen-wrapper">
-            <div className="WelcomePage__welcome-section">
-              <h2>Welcome to Mech</h2>
-            </div>
-            <div className="WelcomePage__notice">
-              <h3>Notice: Currently we only support Toyota, Honda, GMC, Buick, Chevrolet, and Cadillac.</h3>
-            </div>
-            <div id="WelcomePage__login-logout-link">
-              <Link to='/home'>
-                Rev!
-              </Link>
-            </div>
-            <div id="WelcomePage__intro">
-              <ul className="WelcomePage__intro-ul">
-                <li>
-                Mech is a community for car owners and mechanics to discuss problems and solutions based on car manufacturers and diagnostic trouble codes (DTC). <br /><br />Enter your VIN or Make and Model of your car. Then, enter your DTC. Find solutions! Chat with car owners and mechanics.
-                </li>
-              </ul>
-            </div>
+      <main className="WelcomePage__main">
+        <div className="WelcomePage__screen-wrapper">
+          <div className="WelcomePage__welcome-section">
+            <h2>Welcome to Mech</h2>
           </div>
-        </main>
+          <div className="WelcomePage__notice">
+            <h3>
+              Notice: Currently we only support Toyota, Honda, GMC, Buick,
+              Chevrolet, and Cadillac.
+            </h3>
+          </div>
+          <div className="WelcomePage__notice">
+            <h4>
+              You may use<br />
+              l: testaccount@gmail.com / p: @ABCabc123<br />
+              for the login credentials.
+            </h4>
+          </div>
+          <div id="WelcomePage__login-logout-link">
+            <Link to="/home">Rev!</Link>
+          </div>
+          <div id="WelcomePage__intro">
+            <ul className="WelcomePage__intro-ul">
+              <li>
+                Mech is a community for car owners and mechanics to discuss
+                problems and solutions based on car manufacturers and diagnostic
+                trouble codes (DTC). <br />
+                <br />
+                Enter your VIN or Make and Model of your car. Then, enter your
+                DTC. Find solutions! Chat with car owners and mechanics.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
     );
   }
 }
